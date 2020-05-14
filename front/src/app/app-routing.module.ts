8a7515aafcadabcc7c5a9787991ec2daac540e93
jamesclerc <james.clerc@epitech.eu>
2020-05-14 15:37:47 +0200

    adding the auth guard

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuardService } from './auth-guard.service'
+
 import { LoginComponent } from './pages/login/login.component';
 import { NewtaskComponent } from './pages/newtask/newtask.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
 import { EdittaskComponent } from './pages/edittask/edittask.component';
 
+
 const routes: Routes = [
-  { path: '', redirectTo: 'tasks', pathMatch: 'full' },
-  { path: 'tasks', component: TaskViewComponent },
-  { path: 'tasks/newtask', component: NewtaskComponent },
-  {path: 'tasks/edittask/:taskId', component: EdittaskComponent},
+  { path: '', redirectTo: 'tasks', pathMatch: 'full', canActivate : [AuthGuardService] },
+  { path: 'tasks', component: TaskViewComponent , canActivate : [AuthGuardService] },
+  { path: 'tasks/newtask', component: NewtaskComponent , canActivate : [AuthGuardService] },
+  { path: 'tasks/edittask/:taskId', component: EdittaskComponent , canActivate : [AuthGuardService] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
